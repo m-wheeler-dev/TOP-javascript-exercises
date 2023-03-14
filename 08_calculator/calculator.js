c4c1d1f007b0b498baf7e044c553e59a19e0faa0
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2023-03-13 19:11:07 -0700

    Pass all tests for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,64 @@
-const add = function() {
-	
+const add = function (x, y) {
+	return (x += y);
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+	return (x -= y);
 };
 
-const sum = function() {
-	
+const sum = function ([...nums]) {
+	let total = 0;
+	if (nums.length === 0) {
+		return 0;
+	} else if (nums.length === 1) {
+		return nums[0];
+	} else {
+		for (i = 0; i < nums.length; i++) {
+			total += nums[i];
+		}
+	}
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function ([...nums]) {
+	let total = nums[0];
+	if (nums.length === 0) {
+		return 0;
+	} else if (nums.length === 1) {
+		return nums[0];
+	} else {
+		for (i = 1; i < nums.length; i++) {
+			total *= nums[i];
+		}
+	}
+	return total;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+	let total = Math.pow(x, y);
+
+	return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+	let total = 1;
+
+	if (x === 0 || x === 1) {
+		return total;
+	} else {
+		for (i = x; i > 0; i--) {
+			total = total * i;
+		}
+	}
+	return total;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
