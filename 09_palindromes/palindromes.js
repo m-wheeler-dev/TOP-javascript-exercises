ae83a8cf21aeb6799f1832c96c6d40b70197199b
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2023-03-13 19:48:31 -0700

    Pass all palindrome tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let lowerStr = string.toLowerCase().replace(/[^A-Za-z0–9]/g, "");
 
+	let reverseString = lowerStr.split("").reverse().join("");
+
+	return reverseString == lowerStr;
 };
 
 // Do not edit below this line
